8d94d56bab3fc8a016839646d92e7360b51aa279
B2Hacker <brandon.cortes.1690@gmail.com>
2021-06-19 21:44:58 -0500

    Updating Certificates

--- src/components/cerCard.vue
+++ src/components/cerCard.vue
@@ -5,12 +5,13 @@
       <v-row>
         <v-col cols="4" v-for="item in items" :key="item.id">
           <v-card outlined class="mx-auto">
+            <v-card-title>{{item.title}}</v-card-title>
             <v-img
-              class="white--text align-end"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              max-height="auto"
+              max-width="auto"
+              :src="item.src"
             >
-              <v-card-title>{{item.title}}</v-card-title>
+              
             </v-img>
 
             <v-card-subtitle class="pb-0"> {{item.subtitle}} </v-card-subtitle>
@@ -38,44 +39,44 @@ export default {
             {
         id: 1,
         title: "TSU Certificate",
-        subtitle: "1,000 miles of wonder",
+        subtitle: "Diciembre 2020",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/img003.jpg")
       },
       {
         id: 2,
         title: "ITEP Certificate",
         subtitle: "1,000 miles of wonder",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/img002.jpg")
       },
                   {
         id: 3,
         title: "Duolingo Certificate",
         subtitle: "1,000 miles of wonder",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/img001.jpg")
       },
                   {
         id: 4,
         title: "Nuevo Ladero City College Certificate",
         subtitle: "1,000 miles of wonder",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/NLCC.png")
       },
                   {
         id: 5,
         title: "MUNUT Certificate",
         subtitle: "1,000 miles of wonder",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/img004.jpg")
       },
                   {
         id: 6,
         title: "Cisco CyberSecurity Certificate",
         subtitle: "1,000 miles of wonder",
         description: "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-        src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        src: require("../assets/certificates/Cisco.png")
       },
     ],
   }),
